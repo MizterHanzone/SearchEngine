bf660c092123439e27d61c9b9144750ee34ea685
MizterHanzone <155834736+MizterHanzone@users.noreply.github.com>
2025-08-02 23:06:04 +0700

    first

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,308 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
+
+<head>
+    <title>SurfsideMedia</title>
+    <meta charset="utf-8">
+    <meta name="author" content="themesflat.com">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link rel="stylesheet" type="text/css" href="css/animate.min.css">
+    <link rel="stylesheet" type="text/css" href="css/animation.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap-select.min.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" href="font/fonts.css">
+    <link rel="stylesheet" href="icon/style.css">
+    <link rel="shortcut icon" href="images/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" href="images/favicon.ico">
+    <link rel="stylesheet" type="text/css" href="css/sweetalert.min.css">
+    <link rel="stylesheet" type="text/css" href="css/custom.css">
+</head>
+
+<body class="body">
+    <div id="wrapper">
+        <div id="page" class="">
+            <div class="layout-wrap">
+
+                <!-- <div id="preload" class="preload-container">
+    <div class="preloading">
+        <span></span>
+    </div>
+</div> -->
+
+                <div class="section-menu-left">
+                    <div class="box-logo">
+                        <a href="index.html" id="site-logo-inner">
+                            <img class="" id="logo_header" alt="" src="images/logo/logo.png"
+                                data-light="images/logo/logo.png" data-dark="images/logo/logo.png">
+                        </a>
+                        <div class="button-show-hide">
+                            <i class="icon-menu-left"></i>
+                        </div>
+                    </div>
+                </div>
+                <div class="section-content-right">
+
+                    <div class="header-dashboard">
+                        <div class="wrap">
+                            <div class="header-left">
+                                <a href="index-2.html">
+                                    <img class="" id="logo_header_mobile" alt="" src="images/logo/logo.png"
+                                        data-light="images/logo/logo.png" data-dark="images/logo/logo.png"
+                                        data-width="154px" data-height="52px" data-retina="images/logo/logo.png">
+                                </a>
+                                <div class="button-show-hide">
+                                    <i class="icon-menu-left"></i>
+                                </div>
+
+
+                                <form class="form-search flex-grow">
+                                    <fieldset class="name">
+                                        <input type="text" placeholder="Search here..." class="show-search" name="name"
+                                            tabindex="2" value="" aria-required="true" required="">
+                                    </fieldset>
+                                    <div class="button-submit">
+                                        <button class="" type="submit"><i class="icon-search"></i></button>
+                                    </div>
+                                    <div class="box-content-search" id="box-content-search">
+                                        <ul class="mb-24">
+                                            <li class="mb-14">
+                                                <div class="body-title">Top selling product</div>
+                                            </li>
+                                            <li class="mb-14">
+                                                <div class="divider"></div>
+                                            </li>
+                                            <li>
+                                                <ul>
+                                                    <li class="product-item gap14 mb-10">
+                                                        <div class="image no-bg">
+                                                            <img src="images/products/17.png" alt="">
+                                                        </div>
+                                                        <div class="flex items-center justify-between gap20 flex-grow">
+                                                            <div class="name">
+                                                                <a href="product-list.html" class="body-text">Dog Food
+                                                                    Rachael Ray Nutrish®</a>
+                                                            </div>
+                                                        </div>
+                                                    </li>
+                                                    <li class="mb-10">
+                                                        <div class="divider"></div>
+                                                    </li>
+                                                    <li class="product-item gap14 mb-10">
+                                                        <div class="image no-bg">
+                                                            <img src="images/products/18.png" alt="">
+                                                        </div>
+                                                        <div class="flex items-center justify-between gap20 flex-grow">
+                                                            <div class="name">
+                                                                <a href="product-list.html" class="body-text">Natural
+                                                                    Dog Food Healthy Dog Food</a>
+                                                            </div>
+                                                        </div>
+                                                    </li>
+                                                    <li class="mb-10">
+                                                        <div class="divider"></div>
+                                                    </li>
+                                                    <li class="product-item gap14">
+                                                        <div class="image no-bg">
+                                                            <img src="images/products/19.png" alt="">
+                                                        </div>
+                                                        <div class="flex items-center justify-between gap20 flex-grow">
+                                                            <div class="name">
+                                                                <a href="product-list.html" class="body-text">Freshpet
+                                                                    Healthy Dog Food and Cat</a>
+                                                            </div>
+                                                        </div>
+                                                    </li>
+                                                </ul>
+                                            </li>
+                                        </ul>
+                                        <ul class="">
+                                            <li class="mb-14">
+                                                <div class="body-title">Order product</div>
+                                            </li>
+                                            <li class="mb-14">
+                                                <div class="divider"></div>
+                                            </li>
+                                            <li>
+                                                <ul>
+                                                    <li class="product-item gap14 mb-10">
+                                                        <div class="image no-bg">
+                                                            <img src="images/products/20.png" alt="">
+                                                        </div>
+                                                        <div class="flex items-center justify-between gap20 flex-grow">
+                                                            <div class="name">
+                                                                <a href="product-list.html" class="body-text">Sojos
+                                                                    Crunchy Natural Grain Free...</a>
+                                                            </div>
+                                                        </div>
+                                                    </li>
+                                                    <li class="mb-10">
+                                                        <div class="divider"></div>
+                                                    </li>
+                                                    <li class="product-item gap14 mb-10">
+                                                        <div class="image no-bg">
+                                                            <img src="images/products/21.png" alt="">
+                                                        </div>
+                                                        <div class="flex items-center justify-between gap20 flex-grow">
+                                                            <div class="name">
+                                                                <a href="product-list.html" class="body-text">Kristin
+                                                                    Watson</a>
+                                                            </div>
+                                                        </div>
+                                                    </li>
+                                                    <li class="mb-10">
+                                                        <div class="divider"></div>
+                                                    </li>
+                                                    <li class="product-item gap14 mb-10">
+                                                        <div class="image no-bg">
+                                                            <img src="images/products/22.png" alt="">
+                                                        </div>
+                                                        <div class="flex items-center justify-between gap20 flex-grow">
+                                                            <div class="name">
+                                                                <a href="product-list.html" class="body-text">Mega
+                                                                    Pumpkin Bone</a>
+                                                            </div>
+                                                        </div>
+                                                    </li>
+                                                    <li class="mb-10">
+                                                        <div class="divider"></div>
+                                                    </li>
+                                                    <li class="product-item gap14">
+                                                        <div class="image no-bg">
+                                                            <img src="images/products/23.png" alt="">
+                                                        </div>
+                                                        <div class="flex items-center justify-between gap20 flex-grow">
+                                                            <div class="name">
+                                                                <a href="product-list.html" class="body-text">Mega
+                                                                    Pumpkin Bone</a>
+                                                            </div>
+                                                        </div>
+                                                    </li>
+                                                </ul>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </form>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="main-content">
+
+                        <div class="bottom-page">
+                            <div class="body-text">Copyright © 2025 Team2 Search egine</div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/bootstrap-select.min.js"></script>   
+    <script src="js/sweetalert.min.js"></script>    
+    <script src="js/apexcharts/apexcharts.js"></script>
+    <script src="js/main.js"></script>
+    <script>
+        (function ($) {
+
+            var tfLineChart = (function () {
+
+                var chartBar = function () {
+
+                    var options = {
+                        series: [{
+                            name: 'Total',
+                            data: [0.00, 0.00, 0.00, 0.00, 0.00, 273.22, 208.12, 0.00, 0.00, 0.00, 0.00, 0.00]
+                        }, {
+                            name: 'Pending',
+                            data: [0.00, 0.00, 0.00, 0.00, 0.00, 273.22, 208.12, 0.00, 0.00, 0.00, 0.00, 0.00]
+                        },
+                        {
+                            name: 'Delivered',
+                            data: [0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00]
+                        }, {
+                            name: 'Canceled',
+                            data: [0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00]
+                        }],
+                        chart: {
+                            type: 'bar',
+                            height: 325,
+                            toolbar: {
+                                show: false,
+                            },
+                        },
+                        plotOptions: {
+                            bar: {
+                                horizontal: false,
+                                columnWidth: '10px',
+                                endingShape: 'rounded'
+                            },
+                        },
+                        dataLabels: {
+                            enabled: false
+                        },
+                        legend: {
+                            show: false,
+                        },
+                        colors: ['#2377FC', '#FFA500', '#078407', '#FF0000'],
+                        stroke: {
+                            show: false,
+                        },
+                        xaxis: {
+                            labels: {
+                                style: {
+                                    colors: '#212529',
+                                },
+                            },
+                            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+                        },
+                        yaxis: {
+                            show: false,
+                        },
+                        fill: {
+                            opacity: 1
+                        },
+                        tooltip: {
+                            y: {
+                                formatter: function (val) {
+                                    return "$ " + val + ""
+                                }
+                            }
+                        }
+                    };
+
+                    chart = new ApexCharts(
+                        document.querySelector("#line-chart-8"),
+                        options
+                    );
+                    if ($("#line-chart-8").length > 0) {
+                        chart.render();
+                    }
+                };
+
+                /* Function ============ */
+                return {
+                    init: function () { },
+
+                    load: function () {
+                        chartBar();
+                    },
+                    resize: function () { },
+                };
+            })();
+
+            jQuery(document).ready(function () { });
+
+            jQuery(window).on("load", function () {
+                tfLineChart.load();
+            });
+
+            jQuery(window).on("resize", function () { });
+        })(jQuery);
+    </script>
+</body>
+
+</html>
